0e451caac14f508d5d2470fbb887e740d2a4b5fd
cricehomelab <91029276+cricehomelab@users.noreply.github.com>
2022-01-02 10:48:32 -0500

    Add files via upload

--- D10_Docstrings.py
+++ D10_Docstrings.py
@@ -1,8 +1,7 @@
 
 # Docstring is the fist line after a function is called it lives between 3 sets of double quotes and can be multi lined
-
 def format_name(f_name, l_name):
-    """ Takes a first name (f_name) and a last name (l_name) and formats it to the title case. """
+    """ format_name(f_name, l_name) - takes a first name (f_name) and a last name (l_name) and formats it to the title case. """
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     if f_name == "" or l_name == "":
@@ -11,8 +10,6 @@ def format_name(f_name, l_name):
         return "Incomplete first or last name"
     # this is the first return assuming first name and last name were entered.
     return f"{formated_f_name} {formated_l_name}"
-    # second return will never get executed if another return runs before it.
-    return "This will never run."
 
 print(format_name("Jeff", "SMITH"))
 
